2ef3436f1883718b78adb1f55129e9385580bac8
lyeap <lyeapaul@gmail.com>
2024-06-19 15:26:40 -0400

    -- assignment added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+//load enviroment
+dotenv.config();
+
+const db = require("./toy-shop/modules/toys/db");
+
+const app = express();
+
+const port = process.env.PORT || 8888;
+
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","pug");
+
+app.get("/",async (req,res) =>{
+  
+  const toys =await db.getToys();
+  res.render("index",{toys});
+})
+
+app.listen(port, ()=> {
+  console.log(`Server running on http://localhost:${port}`);
+})
+
+
---
+++ toy-shop/modules/toys/db.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const dbUrl = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPWD}@${process.env.DBHOST}`;
+
+const ToySchema = new mongoose.Schema({
+  name: {type:String,required:true},
+  description: {type: String,required:true},
+  price: {type:Number,required:true},
+});
+
+const Toy = mongoose.model("Toy", ToySchema);
+
+async function connect() {
+  await mongoose.connect(dbUrl);
+}
+
+async function getToys() {
+  await connect();
+  return await Toy.find({});
+}
+
+//get toy by id
+async function getToyById(id){
+  await connect();
+  return await Toy.findById(id);
+}
+
+
+
+module.exports = {
+  connect,
+  getToys,
+  getToyById,
+  
+
+
+};
+
